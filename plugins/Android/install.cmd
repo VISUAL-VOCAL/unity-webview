0481915e34bd1a5d9d64b26918d79546636d6c7a
Adam Szofran <adam@visualvocal.com>
2016-10-04 14:28:22 -0700

    Add install.cmd to build on Windows. (Ported from install.sh.)

---
+++ plugins/Android/install.cmd
@@ -0,0 +1,39 @@
+@echo off
+setlocal
+
+if not defined ANDROID_SDK (
+    echo Must set ANDROID_SDK environment variable to the Android SDK directory.
+    goto end
+)
+
+if not defined ANT_HOME (
+    echo Must set ANT_HOME environment variable to the Apache Ant directory.
+    goto end
+)
+
+if not defined UNITY_HOME (
+    echo Must set UNITY_HOME environment variable to the Unity directory.
+    goto end
+)
+
+set ANDROID_SDK=C:\Apps\android-sdk
+set ANT_HOME=c:\apps\apache-ant-1.9.7
+
+path=%path%;%ANT_HOME%\bin;%ANDROID_SDK%\tools
+
+set UNITYLIBS=%UNITY_HOME%\Editor\Data\PlaybackEngines\AndroidPlayer\Variations\mono\Release\Classes\
+set DSTDIR=..\..\build\Packager\Assets\Plugins\Android
+set ANT_OPTS=-Dfile.encoding=UTF8
+call android update project -t android-19 -p .
+mkdir libs
+copy "%UNITYLIBS%\*.*" libs
+call ant "-Djava.compilerargs=-Xlint:deprecation" release
+mkdir %DSTDIR%
+copy bin\classes.jar %DSTDIR%\WebViewPlugin.jar
+call ant clean
+rmdir /s /q libs
+rmdir /s /q res
+del /q proguard-project.txt
+
+:end
+endlocal
