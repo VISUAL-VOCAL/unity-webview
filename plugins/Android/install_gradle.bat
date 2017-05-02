a1cd085876e030984b52a451d0ac90b72aaf365e
Adam Szofran <adam@visualvocal.com>
2017-05-01 22:04:11 -0700

    Patch up install_gradle with paths from my local environment to make builds easier in the future even though the paths will be wrong on other machines.

--- plugins/Android/install_gradle.bat
+++ plugins/Android/install_gradle.bat
@@ -1,8 +1,12 @@
 @echo off
+setlocal
 
-rd /s /q bin
-rd /s /q gradle_build\libs
-rd /s /q gradle_build\src
+set JAVA_HOME=c:\Program Files\Java\jdk1.8.0_131
+path=%path%;%JAVA_HOME%\bin
+
+if exist bin rmdir /s /q bin
+if exist gradle_build\libs rmdir /s /q gradle_build\libs
+if exist gradle_build\src rmdir /s /q gradle_build\src
 
 mkdir bin
 mkdir gradle_build\libs
@@ -10,10 +14,12 @@ mkdir gradle_build\src
 mkdir gradle_build\src\main
 mkdir gradle_build\src\main\java
 
-copy /b "\Program Files\Unity\Editor\Data\PlaybackEngines\AndroidPlayer\Variations\mono\Release\Cla\sses\classes.jar" gradle_build\libs >nul
+copy /b "%ProgramFiles%\Unity\Editor\Data\PlaybackEngines\AndroidPlayer\Variations\mono\Release\Classes\classes.jar" gradle_build\libs >nul
 xcopy /s /e src gradle_build\src\main\java >nul
 copy /b AndroidManifest.xml gradle_build\src\main >nul
 
 call gradlew.bat assembleRelease
 
 jar cf bin\WebViewPlugin.jar -C gradle_build\build\intermediates\classes\release net
+
+endlocal
